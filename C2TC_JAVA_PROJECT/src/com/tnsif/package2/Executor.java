0f188c892aff4fa20b568c61e313e796a094539d
man-2005 <manojnagraj18@gmail.com>
2025-07-31 19:48:05 +0530

    package

---
+++ C2TC_JAVA_PROJECT/src/com/tnsif/package2/Executor.java
@@ -0,0 +1,12 @@
+package com.tnsif.package2;
+
+import com.tnsif.package1.Base;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		Base b=new Base();
+		b.varPublic=12;
+		b.methodPublic();
+	}
+}
